97a25a44fc4b133b44af897612396022cb8d52b4
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-06-26 09:26:47 +0200

    WiP PoC AFHDS3 internal module

--- radio/src/pulses/pulses.cpp
+++ radio/src/pulses/pulses.cpp
@@ -48,6 +48,10 @@
 #include "pulses/afhds2.h"
 #endif
 
+#if defined(AFHDS3)
+#include "pulses/afhds3.h"
+#endif
+
 uint8_t s_pulses_paused = 0;
 ModuleState moduleState[NUM_MODULES];
 InternalModulePulsesData intmodulePulsesData __DMA;
@@ -170,8 +174,8 @@ void getModuleStatusString(uint8_t moduleIdx, char * statusText)
   }
 #endif
 #if defined(AFHDS3)
-  if (moduleIdx == EXTERNAL_MODULE && isModuleAFHDS3(moduleIdx)) {
-    extmodulePulsesData.afhds3.getStatusString(statusText);
+  if (isModuleAFHDS3(moduleIdx)) {
+    afhds3::getStatusString(moduleIdx, statusText);
   }
 #endif
 }
@@ -185,8 +189,8 @@ void getModuleSyncStatusString(uint8_t moduleIdx, char * statusText)
   }
 #endif
 #if defined(AFHDS3)
-  if (moduleIdx == EXTERNAL_MODULE && isModuleAFHDS3(moduleIdx)) {
-    extmodulePulsesData.afhds3.getPowerStatus(statusText);
+  if (isModuleAFHDS3(moduleIdx)) {
+    afhds3::getPowerStatus(moduleIdx, statusText);
   }
 #endif
 }
@@ -194,8 +198,8 @@ void getModuleSyncStatusString(uint8_t moduleIdx, char * statusText)
 #if defined(AFHDS3)
 uint8_t actualAfhdsRunPower(int moduleIndex)
 {
-  if (moduleIndex == EXTERNAL_MODULE && isModuleAFHDS3(moduleIndex)) {
-    return (uint8_t)extmodulePulsesData.afhds3.actualRunPower();
+  if (isModuleAFHDS3(moduleIndex)) {
+    return (uint8_t)afhds3::getActualRunPower(moduleIndex);
   }
   return 0;
 }
