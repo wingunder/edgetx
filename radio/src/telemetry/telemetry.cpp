43f2e97cbc0702af32e71572ba4a6e9ef234e594
Raphael Coeffic <raphael.coeffic@frafos.com>
2022-06-26 09:24:35 +0200

    Move AFHDS3 to new module interface

--- radio/src/telemetry/telemetry.cpp
+++ radio/src/telemetry/telemetry.cpp
@@ -197,7 +197,7 @@ void processTelemetryData(uint8_t data)
 
 #if defined(AFHDS3)
   if (telemetryProtocol == PROTOCOL_TELEMETRY_AFHDS3) {
-    afhds3::processTelemetryData(EXTERNAL_MODULE, data, telemetryRxBuffer, telemetryRxBufferCount, TELEMETRY_RX_PACKET_SIZE);
+    afhds3::processTelemetryData(data, EXTERNAL_MODULE);
     return;
   }
 #endif
@@ -291,7 +291,7 @@ static void pollIntCrossfire()
 }
 #endif
 
-#if defined(PCBNV14)
+#if defined(INTERNAL_MODULE_AFHDS2A)
 static void processFlySkyTelemetryData(uint8_t data, uint8_t idx)
 {
   (void)idx;
@@ -301,6 +301,13 @@ static void processFlySkyTelemetryData(uint8_t data, uint8_t idx)
 static void pollIntAFHDS2A()
 {
   pollIntTelemetry(processFlySkyTelemetryData);
+}
+#endif
+
+#if defined(INTERNAL_MODULE_AFHDS3)
+static void pollIntAFHDS3()
+{
+  pollIntTelemetry(afhds3::processTelemetryData);
 }
 #endif
 
@@ -356,10 +363,16 @@ void telemetryWakeup()
     pollIntCrossfire();
   }
 #endif
-#if defined(PCBNV14)
+#if defined(INTERNAL_MODULE_AFHDS2A)
   //! moduleUpdateActive(INTERNAL_MODULE) &&
   if (isModuleAFHDS2A(INTERNAL_MODULE)) {
     pollIntAFHDS2A();
+  }
+#endif
+#if defined(INTERNAL_MODULE_AFHDS3)
+  //! moduleUpdateActive(INTERNAL_MODULE) &&
+  if (isModuleAFHDS3(INTERNAL_MODULE)) {
+    pollIntAFHDS3();
   }
 #endif
 
